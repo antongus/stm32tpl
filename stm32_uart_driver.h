3ee03222cc85c581b710d8af7b0aedd0c1d073a5
Anton Gusev <antongus@mail.ru>
2015-07-10 11:05:16 +0500

    Finally made ReadData()/WriteData() static

--- stm32_uart_driver.h
+++ stm32_uart_driver.h
@@ -515,8 +515,8 @@ public:
 #else
 	INLINE static uint16_t Status()                 { return USARTx->SR; }
 	INLINE static void ClearStatus(uint32_t flags)  { USARTx->SR &= ~flags; }
-	INLINE uint16_t ReadData()                      { return USARTx->DR; }
-	INLINE void WriteData(uint8_t data)             { USARTx->DR = data; }
+	INLINE static uint16_t ReadData()               { return USARTx->DR; }
+	INLINE static void WriteData(uint8_t data)      { USARTx->DR = data; }
 #endif
 
 	struct RxInterrupt
