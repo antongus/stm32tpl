b681c3bd0556a8782e17bc9b1f2526d74edb13db
Anton Gusev <antongus@mail.ru>
2015-12-29 09:58:49 +0500

    fix bug in atof
    When called with arguments with long precision, atof
    sometomes was return incorrect results. This was due
    integer overflow in small_atoi() subcalls.
    Now calls to small_atoi() replaced with calls to
    small_atoll(), which operates with long long ints.
    This eliminates problem with atof in all allowed
    precision range (up to 10).

--- util.h
+++ util.h
@@ -42,6 +42,7 @@ void strreverse(char* begin, char* end);
 char *str_upr(char *s);
 char* itoa(int value, char* s, int base);
 long small_atoi(char * s);
+long long small_atoll(char * s);
 double small_atof(const char * s);
 uint32_t small_atoh(char * s);
 
