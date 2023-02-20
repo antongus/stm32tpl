5a188bf1bf2fc901d2801e7dae5df54a7ba9fbd3
Anton Gusev <antongus@mail.ru>
2023-02-20 14:22:16 +0500

    add support for stm32l011xx

--- pin.h
+++ pin.h
@@ -36,7 +36,13 @@
 
 #if (defined STM32F2XX) || (defined STM32F4XX) || (defined STM32F40_41xxx) || (defined STM32F427_437xx) || (defined STM32F429_439xx) || (defined STM32F401xx)
 #  include "pin_stm32F4xx.h"
-#elif (defined STM32L051xx) || (defined STM32L052xx) || (defined STM32L053xx) || (defined STM32L061xx) || (defined STM32L062xx) || (defined STM32L063xx)
+#elif (defined STM32L010x4) || (defined STM32L010x6) || (defined STM32L010x8) || (defined STM32L010xB) || \
+    (defined STM32L011xx) || (defined STM32L021xx) || \
+    (defined STM32L031xx) || (defined STM32L041xx) || \
+    (defined STM32L051xx) || (defined STM32L052xx) || (defined STM32L053xx) || \
+    (defined STM32L062xx) || (defined STM32L063xx) || \
+    (defined STM32L071xx) || (defined STM32L072xx) || (defined STM32L073xx) || \
+    (defined STM32L081xx) || (defined STM32L082xx) || (defined STM32L083xx)
 #  include "pin_stm32L0xx.h"
 #elif (defined STM32L100xB) || (defined STM32L100xBA) || (defined STM32L100xC) || \
     (defined STM32L151xB) || (defined STM32L151xBA) || (defined STM32L151xC) || (defined STM32L151xCA) || (defined STM32L151xD) || (defined STM32L151xDX) || (defined STM32L151xE) || \
--- stm32.h
+++ stm32.h
@@ -58,7 +58,13 @@
 #  define STM32F4XX
 #  include "CMSIS/stm32f4xx.h"
 #  define STM32TPL_F2xxF4xx
-#elif (defined STM32L051xx) || (defined STM32L052xx) || (defined STM32L053xx) || (defined STM32L061xx) || (defined STM32L062xx) || (defined STM32L063xx)
+#elif (defined STM32L010x4) || (defined STM32L010x6) || (defined STM32L010x8) || (defined STM32L010xB) || \
+    (defined STM32L011xx) || (defined STM32L021xx) || \
+    (defined STM32L031xx) || (defined STM32L041xx) || \
+    (defined STM32L051xx) || (defined STM32L052xx) || (defined STM32L053xx) || \
+    (defined STM32L062xx) || (defined STM32L063xx) || \
+    (defined STM32L071xx) || (defined STM32L072xx) || (defined STM32L073xx) || \
+    (defined STM32L081xx) || (defined STM32L082xx) || (defined STM32L083xx)
 #  define STM32TPL_STM32L0XX
 #  include "CMSIS/stm32l0xx.h"
 #elif (defined STM32L100xB) || (defined STM32L100xBA) || (defined STM32L100xC) || \
